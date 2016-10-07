720767f99cf16f506b7616b88430a1cc3b6ff5f5
guillo <you@example.com>
2016-10-07 02:17:23 +0200

    Adding dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,13 @@ maintainer_email 'guillofar@gmail.com'
 license 'Apache 2.0'
 description 'Installs and Configures node.js, deploys the app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/guillefar/SurveysApiCookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/guillefar/SurveysApiCookbook/issues' if respond_to?(:issues_url)
 version '0.0.1'
 
 depends 'yum-epel'
 depends 'build-essential'
+depends 'dmg'
 depends 'ark'
 depends 'apt', '>= 2.9.1'
 depends 'homebrew'
